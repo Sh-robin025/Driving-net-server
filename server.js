8bb8a2a4364233168536e8785b9b47a643a237aa
Shahadat Robin <sh.robin025@gmail.com>
2021-04-20 23:18:41 +0600

    delete route added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const { json } = require('express');
+const { ObjectID } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5050;
 
@@ -34,6 +35,13 @@ client.connect(err => {
         topBannerCollection.find()
             .toArray((err, items) => res.send(items))
     })
+    app.delete('/deleteBanner/:id', (req, res) => {
+        topBannerCollection.deleteOne({ _id: ObjectID(req.params.id) })
+            .then(result => {
+                result.deletedCount === 1 && res.send("delete success")
+            })
+            .catch(err => console.log("delete err : ", err))
+    })
     app.post('/addService', (req, res) => {
         serviceCollection.insertOne(req.body.body)
             .then(result => result.insertedCount > 0 && res.sendStatus(200))
@@ -43,6 +51,13 @@ client.connect(err => {
         serviceCollection.find()
             .toArray((err, items) => res.send(items))
     })
+    app.delete('/deleteService/:id', (req, res) => {
+        serviceCollection.deleteOne({ _id: ObjectID(req.params.id) })
+            .then(result => {
+                result.deletedCount === 1 && res.send("delete success")
+            })
+            .catch(err => console.log("delete err : ", err))
+    })
     app.post('/addCourse', (req, res) => {
         courseCollection.insertOne(req.body.body)
             .then(result => result.insertedCount > 0 && res.sendStatus(200))
@@ -51,6 +66,18 @@ client.connect(err => {
     app.get('/courses', (req, res) => {
         courseCollection.find()
             .toArray((err, items) => res.send(items))
+    })
+    app.delete('/deleteCourse/:id', (req, res) => {
+        courseCollection.deleteOne({ _id: ObjectID(req.params.id) })
+            .then(result => {
+                result.deletedCount === 1 && res.send("delete success")
+            })
+            .catch(err => console.log("delete err : ", err))
+    })
+    app.post('/addAdmin', (req, res) => {
+        adminCollection.insertOne(req.body.body)
+            .then(result => result.insertedCount > 0 && res.sendStatus(200))
+            .catch(err => console.log("admin add err : ", err))
     })
     app.post('/addUser', (req, res) => {
         adminCollection.find({ email: req.body.body.email })
@@ -70,8 +97,6 @@ client.connect(err => {
 
     })
     app.get('/user/:email', (req, res) => {
-        console.log(req.params.email)
-        // userCollection.find({ email: req.params.email })
         adminCollection.find({ email: req.params.email })
             .toArray((err, admin) => {
                 admin.length !== 0 && res.send(admin)
@@ -80,6 +105,9 @@ client.connect(err => {
                         .toArray((err, user) => res.send(user))
                 }
             })
+    })
+    app.post('/addOrder', (req, res) => {
+        console.log(req.body.body)
     })
 });
 
